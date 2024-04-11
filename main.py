7b2b7ef10b06aa12b1bb1f3cc13182f1f272df5c
Anotn <antonsizikov@yandex.ru>
2024-04-11 13:11:29 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,66 @@
+import flet
+from flet import IconButton, Page, Row, Column, TextField, icons, Text, TextThemeStyle
+
+def main(page: Page):
+    page.title = "Flet counter example"
+    page.vertical_alignment = "center"
+    page.window_height = 400
+    page.window_width = 500
+    
+    num_from = TextField(value="", text_align="right", width=100)
+    num_to = TextField(value="", text_align="right", width=100)
+    num_count = TextField(value="", text_align="right", width=100)
+    
+    page.add(
+        Row(
+            [
+            Text("Randimizer", theme_style=TextThemeStyle.DISPLAY_SMALL),
+            ],
+            alignment="center",
+        ),
+        Row(
+            [
+                Text("From", theme_style=TextThemeStyle.TITLE_MEDIUM),
+                Text("To", theme_style=TextThemeStyle.TITLE_MEDIUM),
+                Text("Count", theme_style=TextThemeStyle.TITLE_MEDIUM),
+            ],
+            alignment="center",
+        ),
+        Row(
+            [
+                num_from,
+                num_to,
+                num_count,
+            ],
+            alignment="center",
+        ),
+        
+        Row(
+            [
+                Column(
+                    [
+                        Text("From", theme_style=TextThemeStyle.TITLE_MEDIUM),
+                        num_from,
+                    ],
+                    alignment="center",
+                ),
+                Column(
+                    [
+                        Text("To", theme_style=TextThemeStyle.TITLE_MEDIUM),
+                        num_to,
+                    ],
+                    alignment="center",
+                ),
+                Column(
+                    [
+                        Text("Count", theme_style=TextThemeStyle.TITLE_MEDIUM),
+                        num_count,
+                    ],
+                    alignment="center",
+                ),
+            ],
+            alignment="center",
+        ),
+    )
+
+flet.app(target=main)
