cf834fd5322aee884532cb7f69cc9daedbb72ecd
Anotn <antonsizikov@yandex.ru>
2024-04-11 18:40:45 +0300

    add default values

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ def main(page: Page):
     page.window_height = 400
     page.window_width = 500
     
-    start_num = TextField(label="From", text_align="right", width=100, input_filter=NumbersOnlyInputFilter(), keyboard_type=KeyboardType.NUMBER)
-    end_num = TextField(label="To", text_align="right", width=100, input_filter=NumbersOnlyInputFilter(), keyboard_type=KeyboardType.NUMBER)
-    quantity = TextField(label="Count", text_align="right", width=100, input_filter=NumbersOnlyInputFilter(), keyboard_type=KeyboardType.NUMBER)
+    start_num = TextField(label="From", value="1", text_align="right", width=100, input_filter=NumbersOnlyInputFilter(), keyboard_type=KeyboardType.NUMBER)
+    end_num = TextField(label="To", value="10", text_align="right", width=100, input_filter=NumbersOnlyInputFilter(), keyboard_type=KeyboardType.NUMBER)
+    quantity = TextField(label="Count", value="1", text_align="right", width=100, input_filter=NumbersOnlyInputFilter(), keyboard_type=KeyboardType.NUMBER)
 
     def randomizer(e):
         i = 1
